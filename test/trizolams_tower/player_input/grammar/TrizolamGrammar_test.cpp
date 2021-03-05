b9679859088f2c8098f46c3d96142d3edf89f176
the-camputer <mynichor@gmail.com>
2021-03-05 08:04:50 -0500

    Finished unit tests for TrizolamGrammar

--- test/trizolams_tower/player_input/grammar/TrizolamGrammar_test.cpp
+++ test/trizolams_tower/player_input/grammar/TrizolamGrammar_test.cpp
@@ -55,25 +55,88 @@ TEST_CASE("Command Tests")
 
     SECTION("PutInInventoryCommand")
     {
+        std::vector<std::string> s_complete_opt_include = {"pick", "up", "body"};
+        auto complete_opt_included = EarleyParser::build_items(grammar, s_complete_opt_include);
+
+        std::vector<std::string> s_complete_opt_not_included = {"take", "pencil"};
+        auto complete_opt_not_included = EarleyParser::build_items(grammar, s_complete_opt_not_included);
+        // TODO: See handler for incompletes that are grammatically correct
+
+        REQUIRE(EarleyParser::diagnose(*complete_opt_included, grammar, s_complete_opt_not_included) ==
+                RECOGNITION_STATUS::COMPLETE);
+        REQUIRE(EarleyParser::diagnose(*complete_opt_not_included, grammar, s_complete_opt_not_included) ==
+                RECOGNITION_STATUS::COMPLETE);
     }
 
     SECTION("LogCommand")
     {
+        std::vector<std::string> s_complete_full = {"show", "log"};
+        auto complete_full = EarleyParser::build_items(grammar, s_complete_full);
+
+        std::vector<std::string> s_complete_slim = {"log"};
+        auto complete_slim = EarleyParser::build_items(grammar, s_complete_slim);
+
+        std::vector<std::string> s_incomplete = {"show"};
+        auto incomplete = EarleyParser::build_items(grammar, s_incomplete);
+
+        REQUIRE(EarleyParser::diagnose(*complete_full, grammar, s_complete_full) == RECOGNITION_STATUS::COMPLETE);
+        REQUIRE(EarleyParser::diagnose(*complete_slim, grammar, s_complete_slim) == RECOGNITION_STATUS::COMPLETE);
+        REQUIRE(EarleyParser::diagnose(*incomplete, grammar, s_incomplete) == RECOGNITION_STATUS::INCOMPLETE);
     }
 
     SECTION("ResetCommand")
     {
+        std::vector<std::string> s_complete_full = {"reset", "game"};
+        auto complete_full = EarleyParser::build_items(grammar, s_complete_full);
+
+        std::vector<std::string> s_complete_slim = {"reset"};
+        auto complete_slim = EarleyParser::build_items(grammar, s_complete_slim);
+
+        std::vector<std::string> s_incomplete = {"game"};
+        auto incomplete = EarleyParser::build_items(grammar, s_incomplete);
+
+        REQUIRE(EarleyParser::diagnose(*complete_full, grammar, s_complete_full) == RECOGNITION_STATUS::COMPLETE);
+        REQUIRE(EarleyParser::diagnose(*complete_slim, grammar, s_complete_slim) == RECOGNITION_STATUS::COMPLETE);
+        REQUIRE(EarleyParser::diagnose(*incomplete, grammar, s_incomplete) == RECOGNITION_STATUS::INCOMPLETE);
     }
 
     SECTION("LoadCommand")
     {
+        std::vector<std::string> s_complete_full = {"load", "game"};
+        auto complete_full = EarleyParser::build_items(grammar, s_complete_full);
+
+        std::vector<std::string> s_complete_slim = {"load"};
+        auto complete_slim = EarleyParser::build_items(grammar, s_complete_slim);
+
+        REQUIRE(EarleyParser::diagnose(*complete_full, grammar, s_complete_full) == RECOGNITION_STATUS::COMPLETE);
+        REQUIRE(EarleyParser::diagnose(*complete_slim, grammar, s_complete_slim) == RECOGNITION_STATUS::COMPLETE);
     }
 
     SECTION("SaveCommand")
     {
+        std::vector<std::string> s_complete_full = {"save", "game"};
+        auto complete_full = EarleyParser::build_items(grammar, s_complete_full);
+
+        std::vector<std::string> s_complete_slim = {"save"};
+        auto complete_slim = EarleyParser::build_items(grammar, s_complete_slim);
+
+        REQUIRE(EarleyParser::diagnose(*complete_full, grammar, s_complete_full) == RECOGNITION_STATUS::COMPLETE);
+        REQUIRE(EarleyParser::diagnose(*complete_slim, grammar, s_complete_slim) == RECOGNITION_STATUS::COMPLETE);
     }
 
     SECTION("MovementCommand & Direction")
     {
+        std::vector<std::string> s_complete_full = {"go", "north"};
+        auto complete_full = EarleyParser::build_items(grammar, s_complete_full);
+
+        std::vector<std::string> s_complete_slim = {"go", "s"};
+        auto complete_slim = EarleyParser::build_items(grammar, s_complete_slim);
+
+        std::vector<std::string> s_direction_only = {"w"};
+        auto direction_only = EarleyParser::build_items(grammar, s_direction_only);
+
+        REQUIRE(EarleyParser::diagnose(*complete_full, grammar, s_complete_full) == RECOGNITION_STATUS::COMPLETE);
+        REQUIRE(EarleyParser::diagnose(*complete_slim, grammar, s_complete_slim) == RECOGNITION_STATUS::COMPLETE);
+        REQUIRE(EarleyParser::diagnose(*direction_only, grammar, s_direction_only) == RECOGNITION_STATUS::COMPLETE);
     }
 }
