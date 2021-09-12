f4b277bed1bf533a3ceb73f8ea0f29d2796e9a00
the-camputer <mynichor@gmail.com>
2021-09-12 13:56:09 -0400

    GH-8 hooked scene manager and game manager into game

--- src/trizolams_tower/TrizolamsTower.cpp
+++ src/trizolams_tower/TrizolamsTower.cpp
@@ -1,8 +1,8 @@
 #include "bestinshow/engine/grammar/EarleyParser.h"
 #include "bestinshow/engine/InputProcessor.h"
 #include "trizolams_tower/player_input/grammar/TrizolamGrammar.h"
-// #include "trizolams_tower/managers/GameManager.h"
-// #include "trizolams_tower/managers/SceneManager.h"
+#include "./managers/GameManager.h"
+#include "trizolams_tower/managers/SceneManager.h"
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/dll.hpp>
@@ -25,19 +25,32 @@
         update state with results
 */
 
+const std::string scenes_file = "resources/Scenes.yml";
+const std::string program_root = boost::dll::program_location().parent_path().parent_path().c_str();
+const std::string scenes_file_path = program_root + "/" + scenes_file;
+
 std::string get_input();
 
 int main()
 {
-    spdlog::info("cwd: {}", boost::dll::program_location().parent_path());
+    const std::string program_root = boost::dll::program_location().parent_path().parent_path().c_str();
     Grammar game_grammar = TrizolamGrammar::get_instance();
     InputProcessor input_processor{game_grammar};
 
-    // GameManager game_manager{};
-    // SceneManager scene_manager{""}
+    GameManager game_manager{};
+    SceneManager scene_manager{scenes_file_path};
 
-    // TODO: Replace with initial scene description
-    std::cout << "Welcome to Trizolam's Tower!" << std::endl;
+    std::string starting_pos = scene_manager.construct_scenes();
+
+    game_manager.set_player_position(starting_pos);
+
+    std::cout
+        << "Welcome to Trizolam's Tower!\n"
+        << std::endl;
+    std::cout
+        << scene_manager.get_scene_description(game_manager.get_player_position())
+        << std::endl;
+
     while (true)
     {
         std::string player_input = get_input();
